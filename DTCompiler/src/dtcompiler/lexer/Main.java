96d5dcf0a2ababfd2b9f767f9a0603101e2d9c76
Dimitri Distefano <dimitridistefano@ufl.edu>
2022-01-30 20:30:44 -0500

    added a lot of stuff to lexer and some handy testing stuff to main. very based

--- DTCompiler/src/dtcompiler/lexer/Main.java
+++ DTCompiler/src/dtcompiler/lexer/Main.java
@@ -7,13 +7,17 @@ public class Main {
 
 	public static void main(String[] args) throws LexicalException {
 		// TODO Auto-generated method stub
-		
-		
-		Lexer textLexer = new Lexer("abc*asd*afdg*uiy");
-		
-		for(int i = 0; i< 5; i++)
-		System.out.println(textLexer.next().getText());
-		
+
+		Lexer textLexer = new Lexer("""
+				int "yo
+				
+				y"
+				== = <- >= < >
+				yoyo 01.123""");
+
+		for (int i = 0; i < 18; i++)
+			System.out.println(textLexer.peek().getText() + "\t\ttype: " + textLexer.peek().getKind() + "\t\tloc: " + textLexer.next().getSourceLocation());
+
 	}
 
 }
--- DTCompiler/src/dtcompiler/lexer/impl/Lexer.java
+++ DTCompiler/src/dtcompiler/lexer/impl/Lexer.java
@@ -14,27 +14,28 @@ public class Lexer implements ILexer {
 
 	List<Token> tokens;
 	int position;
-	
-	final HashSet<String> type = new HashSet<>(Arrays.asList("string", "int", "float", "boolean",
-			"color", "image", "void"));
+
+	final HashSet<String> type = new HashSet<>(
+			Arrays.asList("string", "int", "float", "boolean", "color", "image", "void"));
 	final HashSet<String> image_op = new HashSet<>(Arrays.asList("getWidth", "getHeight"));
 	final HashSet<String> color_op = new HashSet<>(Arrays.asList("getRed", "getGreen", "getBlue"));
-	final HashSet<String> color_const = new HashSet<>(Arrays.asList("BLACK", "BLUE", "CYAN", "DARK_GRAY",
-			"GRAY", "GREEN", "LIGHT_GRAY", "MAGENTA", "ORANGE", "PINK", "RED", "WHITE", "YELLOW"));
+	final HashSet<String> color_const = new HashSet<>(Arrays.asList("BLACK", "BLUE", "CYAN", "DARK_GRAY", "GRAY",
+			"GREEN", "LIGHT_GRAY", "MAGENTA", "ORANGE", "PINK", "RED", "WHITE", "YELLOW"));
 	final HashSet<String> boolean_lit = new HashSet<>(Arrays.asList("true", "false"));
 
-
 	public static enum State {
-		START, IN_IDENT, IS_ERROR
+		START, IN_IDENT, IS_ERROR, DIGIT, START_ZERO, HAVE_DOT, IS_LT, IS_GT, IS_EQ, IN_STRING, IN_STRING_ESC
 	}
 
-	public Lexer(String input) {
+	public Lexer(String input) throws LexicalException {
 		tokens = new ArrayList<Token>();
 		position = 0;
 
 		State state = State.START;
 		int line = 0;
 		int col = 0;
+		int tokenLine = 0;
+		int tokenCol = 0;
 		String text = "";
 
 		for (int i = 0; i < input.length(); i++) {
@@ -42,40 +43,211 @@ public class Lexer implements ILexer {
 
 			switch (state) {
 
-			case START -> {
+			case START -> { // where the state machine starts
+				tokenLine = line;
+				tokenCol = col;
 				text = "";
 				if ((curr >= 'a' && curr <= 'z') || (curr >= 'A' && curr <= 'Z') || curr == '_' || curr == '$') {
 					state = State.IN_IDENT;
 					text += curr;
 					col++;
+				} else if (curr >= '1' && curr <= '9') {
+					state = State.DIGIT;
+					text += curr;
+					col++;
+				} else if (curr == '0') {
+					state = State.START_ZERO;
+					text += curr;
+					col++;
+				} else if (curr == ' ') {
+					col++;
+				} else if (curr == '>') { // handles gt and gteq
+					col++;
+					text += curr;
+					state = State.IS_GT;
+				} else if (curr == '<') { // handles lt and lteq
+					col++;
+					text += curr;
+					state = State.IS_LT;
+				} else if (curr == '=') { // handles assign and equals
+					col++;
+					text += curr;
+					state = State.IS_EQ;
+				} else if (curr == '\n') {
+					line++;
+					col = 0;
+				} else if (curr == '\"') {
+					state = State.IN_STRING;
+					text += curr;
+					col++;
+				} else {
+					tokens.add(new Token(Kind.ERROR, "Invalid token", tokenLine, tokenCol));
+					i = input.length(); // breaks the loops=
+					// throw new LexicalException("Invalid token");
 				}
+
 			}
-			case IN_IDENT -> {  //handles both reserved and identifier tokens
+
+			case IN_IDENT -> { // handles both reserved and identifier tokens
+
 				if ((curr >= 'a' && curr <= 'z') || (curr >= 'A' && curr <= 'Z') || curr == '_' || curr == '$'
 						|| (curr >= '0' && curr <= '9')) {
 					text += curr;
 					col++;
 				} else {
-					tokens.add(new Token(checkReserved(text), text, line, col));
+					tokens.add(new Token(checkReserved(text), text, tokenLine, tokenCol));
 					state = State.START;
+					i--;
 				}
-				if(i == input.length()-1) {
-					tokens.add(new Token(checkReserved(text), text, line, col));
+				if (i == input.length() - 1) {
+					tokens.add(new Token(checkReserved(text), text, tokenLine, tokenCol));
+				}
+
+			}
+
+			case DIGIT -> {
+
+				if (curr >= '0' && curr <= '9') {
+					text += curr;
+					col++;
+				} else if (curr == '.') {
+					state = State.HAVE_DOT;
+					text += curr;
+					col++;
+				} else {
+					tokens.add(new Token(Kind.INT_LIT, text, tokenLine, tokenCol));
+					state = State.START;
+					i--;
+				}
+				if (i == input.length() - 1) {
+					tokens.add(new Token(Kind.INT_LIT, text, tokenLine, tokenCol));
+				}
+
+			}
+
+			case START_ZERO -> {
+
+				if (curr == '.') {
+					state = State.HAVE_DOT;
+					text += curr;
+					col++;
+				} else {
+					tokens.add(new Token(Kind.INT_LIT, text, tokenLine, tokenCol));
+					state = State.START;
+					i--;
+				}
+				if (i == input.length() - 1) {
+					tokens.add(new Token(Kind.INT_LIT, text, tokenLine, tokenCol));
 				}
 			}
-			/*
-			case HAVE_ZERO -> {
-			}
+
 			case HAVE_DOT -> {
+
+				if (curr >= '0' && curr <= '9') {
+					text += curr;
+					col++;
+				} else {
+					tokens.add(new Token(Kind.FLOAT_LIT, text, tokenLine, tokenCol));
+					state = State.START;
+					i--;
+				}
+				if (i == input.length() - 1) {
+					tokens.add(new Token(Kind.FLOAT_LIT, text, tokenLine, tokenCol));
+				}
 			}
-			*/
+
+			case IS_GT -> {
+
+				if (curr == '=') {
+					text += curr;
+					tokens.add(new Token(Kind.GE, text, tokenLine, tokenCol));
+					state = State.START;
+					col++;
+				} else {
+					tokens.add(new Token(Kind.GT, text, tokenLine, tokenCol));
+					state = State.START;
+					i--;
+				}
+				if (i == input.length() - 1) {
+					tokens.add(new Token(Kind.GT, text, tokenLine, tokenCol));
+				}
 			}
-			
+
+			case IN_STRING -> {
+				if(curr == '\n') {
+					text += curr;
+					col = 0;
+					line++;
+				} else if(curr != '\\' && curr != '\"') {
+					text += curr;
+					col++;
+				} else if (curr == '\"') {
+					text += curr;
+					col++;
+					tokens.add(new Token(Kind.STRING_LIT, text, tokenLine, tokenCol));
+					state = State.START;
+				} else if (curr == '\\') {
+					text += curr;
+					col++;
+					state = State.IN_STRING_ESC;
+				}
+			}
+			case IN_STRING_ESC -> {
+				if (curr == 'b' || curr == 't' || curr == 'n' || curr == 'f' || curr == 'r' || curr == '\"'
+						|| curr == '\'' || curr == '\\') {
+					text += curr;
+					state = State.IN_STRING;
+				} else {
+					tokens.add(new Token(Kind.ERROR, text, tokenLine, tokenCol));
+					i = input.length();
+				}
+			}
+
+			case IS_LT -> {
+				if (curr == '=') {
+					text += curr;
+					tokens.add(new Token(Kind.LE, text, tokenLine, tokenCol));
+					state = State.START;
+					col++;
+				} else if (curr == '-') {
+					text += curr;
+					tokens.add(new Token(Kind.LARROW, text, tokenLine, tokenCol));
+					state = State.START;
+					col++;
+				} else {
+					tokens.add(new Token(Kind.LT, text, tokenLine, tokenCol));
+					state = State.START;
+					i--;
+				}
+				if (i == input.length() - 1) {
+					tokens.add(new Token(Kind.LT, text, tokenLine, tokenCol));
+				}
+			}
+
+			case IS_EQ -> {
+				if (curr == '=') {
+					text += curr;
+					tokens.add(new Token(Kind.EQUALS, text, tokenLine, tokenCol));
+					state = State.START;
+					col++;
+				} else {
+					tokens.add(new Token(Kind.ASSIGN, text, tokenLine, tokenCol));
+					state = State.START;
+					i--;
+				}
+				if (i == input.length() - 1) {
+					tokens.add(new Token(Kind.ASSIGN, text, tokenLine, tokenCol));
+				}
+			}
+
+			}
 		}
 	}
 
 	@Override
 	public IToken next() throws LexicalException {
+		if (position >= tokens.size())
+			throw new LexicalException("Reached end of tokens.");
 		IToken curr = tokens.get(position);
 		position++;
 		return curr;
@@ -83,6 +255,8 @@ public class Lexer implements ILexer {
 
 	@Override
 	public IToken peek() throws LexicalException {
+		if (position >= tokens.size())
+			throw new LexicalException("Reached end of tokens.");
 		return tokens.get(position);
 	}
 
@@ -97,16 +271,18 @@ public class Lexer implements ILexer {
 			return Kind.COLOR_CONST;
 		} else if (boolean_lit.contains(input)) {
 			return Kind.BOOLEAN_LIT;
-		} else if (input == "if") {
+		} else if (input.equals("if")) {
 			return Kind.KW_IF;
-		} else if (input == "fi") {
+		} else if (input.equals("fi")) {
 			return Kind.KW_FI;
-		} else if (input == "else") {
+		} else if (input.equals("else")) {
 			return Kind.KW_ELSE;
-		} else if (input == "write") {
+		} else if (input.equals("write")) {
 			return Kind.KW_WRITE;
-		} else if (input == "console") {
+		} else if (input.equals("console")) {
 			return Kind.KW_CONSOLE;
+		} else if (input.equals("return")) {
+			return Kind.RETURN;
 		}
 		return Kind.IDENT;
 	}
