09960d6e6b0a47ad47681da81a4766e70c075bb1
Tom Liraz <tomliraz@gmail.com>
2022-04-05 18:29:46 -0400

    Added import to component factory.

--- DTCompiler/src/edu/ufl/cise/plc/codegenerator/CodeGenVisitor.java
+++ DTCompiler/src/edu/ufl/cise/plc/codegenerator/CodeGenVisitor.java
@@ -1,7 +1,161 @@
 package edu.ufl.cise.plc.codegenerator;
 
 import edu.ufl.cise.plc.ast.ASTVisitor;
+import edu.ufl.cise.plc.ast.AssignmentStatement;
+import edu.ufl.cise.plc.ast.BinaryExpr;
+import edu.ufl.cise.plc.ast.BooleanLitExpr;
+import edu.ufl.cise.plc.ast.ColorConstExpr;
+import edu.ufl.cise.plc.ast.ColorExpr;
+import edu.ufl.cise.plc.ast.ConditionalExpr;
+import edu.ufl.cise.plc.ast.ConsoleExpr;
+import edu.ufl.cise.plc.ast.Dimension;
+import edu.ufl.cise.plc.ast.FloatLitExpr;
+import edu.ufl.cise.plc.ast.IdentExpr;
+import edu.ufl.cise.plc.ast.IntLitExpr;
+import edu.ufl.cise.plc.ast.NameDef;
+import edu.ufl.cise.plc.ast.NameDefWithDim;
+import edu.ufl.cise.plc.ast.PixelSelector;
+import edu.ufl.cise.plc.ast.Program;
+import edu.ufl.cise.plc.ast.ReadStatement;
+import edu.ufl.cise.plc.ast.ReturnStatement;
+import edu.ufl.cise.plc.ast.StringLitExpr;
+import edu.ufl.cise.plc.ast.UnaryExpr;
+import edu.ufl.cise.plc.ast.UnaryExprPostfix;
+import edu.ufl.cise.plc.ast.VarDeclaration;
+import edu.ufl.cise.plc.ast.WriteStatement;
 
 public class CodeGenVisitor implements ASTVisitor {
+
+	@Override
+	public Object visitBooleanLitExpr(BooleanLitExpr booleanLitExpr, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitStringLitExpr(StringLitExpr stringLitExpr, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitIntLitExpr(IntLitExpr intLitExpr, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitFloatLitExpr(FloatLitExpr floatLitExpr, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitColorConstExpr(ColorConstExpr colorConstExpr, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitConsoleExpr(ConsoleExpr consoleExpr, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitColorExpr(ColorExpr colorExpr, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitUnaryExpr(UnaryExpr unaryExpression, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitBinaryExpr(BinaryExpr binaryExpr, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitIdentExpr(IdentExpr identExpr, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitConditionalExpr(ConditionalExpr conditionalExpr, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitDimension(Dimension dimension, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitPixelSelector(PixelSelector pixelSelector, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitAssignmentStatement(AssignmentStatement assignmentStatement, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitWriteStatement(WriteStatement writeStatement, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitReadStatement(ReadStatement readStatement, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitProgram(Program program, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitNameDef(NameDef nameDef, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitNameDefWithDim(NameDefWithDim nameDefWithDim, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitReturnStatement(ReturnStatement returnStatement, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitVarDeclaration(VarDeclaration declaration, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitUnaryExprPostfix(UnaryExprPostfix unaryExprPostfix, Object arg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
